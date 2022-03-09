513a872fa332dc57d12965e4c6e5516b084db796
Maja Wroblewska <majawroblewska23@gmail.com>
2022-03-09 15:34:29 +0100

    dodane 09-03-2022 panel admina- rejestracja

--- sklepApp/models.py
+++ sklepApp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
 class Kategoria(models.Model):
     nazwa= models.CharField(max_length=100)
 
@@ -10,23 +13,23 @@ class Kategoria(models.Model):
 #-------------------------------------------------------------------------------
 class Produkt(models.Model):
     nazwa= models.CharField(max_length=200)
-    kategoria_id= models.ForeignKey(Kategoria, on_delete=models.CASCADE)
-    opis= models.TextField()
-    zdjecie= models.ImageField(upload_to ='uploads/', default='abc', null=True)
+    kategoria= models.ForeignKey(Kategoria, on_delete=models.CASCADE)
+    opis= models.TextField(default='opis_domyślny')
+    zdjecie= models.ImageField(upload_to ='moja_sciezka', default='domyslna_sciezka', null=True)
     ilosc_w_magazynie= models.IntegerField(default=0)
     cena= models.DecimalField(max_digits=7, decimal_places=2, default=1.00)
-    data_dodania= models.DateTimeField(auto_now_add=True)
-    data_modyfikacji= models.DateTimeField(auto_now_add=True)
+    data_dodania= models.DateTimeField(auto_now_add=True) #podczas tworzenia
+    data_modyfikacji= models.DateTimeField(auto_now=True) #przy zapisie
 
     def __str__(self):
-        return f'{self.nazwa}-kat= {self.kategoria_id}- cena= {self.cena} -stan={self.ilosc_w_magazynie} {self.zdjecie} -{self.data_dodania}/{self.data_modyfikacji}'
+        return f'{self.nazwa} : img={self.zdjecie} : stan={self.ilosc_w_magazynie} : cena= {self.cena} : data dod/mod= {self.data_dodania}/{self.data_modyfikacji}'
 #-------------------------------------------------------------------------------
 
 class Email(models.Model):
-    mail= models.EmailField()
+    email= models.EmailField()
 
     def __str__(self):
-        return f'{self.mail}'
+        return f'{self.email}'
 #-------------------------------------------------------------------------------
 
 class Adres(models.Model):
@@ -43,15 +46,15 @@ class User(models.Model):
     imie= models.CharField(max_length=100)
     nazwisko= models.CharField(max_length=200)
     login= models.CharField(max_length=100)
-    email_id= models.ForeignKey(Email, on_delete=models.CASCADE)
-    adres_id= models.ForeignKey(Adres, on_delete=models.CASCADE)
+    email= models.ForeignKey(Email, on_delete=models.CASCADE)
+    adres= models.ForeignKey(Adres, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.imie} {self.nazwisko} - {self.login}'
 # -------------------------------------------------------------------------------
 class Koszyk_login(models.Model):
     nr_zamowienia= models.IntegerField()
-    user_id=models.ForeignKey(User, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     produkt= models.ManyToManyField(Produkt)
 
     def __str__(self):
